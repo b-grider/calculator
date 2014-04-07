47c7a204fabb9edc237fae876a16e22343296303
dezayasa <dezayasa@ufl.edu>
2014-04-07 13:51:26 -0400

    Update Help.cpp

--- Help.cpp
+++ Help.cpp
@@ -1,9 +1,28 @@
-#include "Help.h"
-Help::printMenu() {
-    
-}
-Help::move(int navigate) {
-    
-}
+#include "help.h"
 
+using namespace std;
 
+help::printMenu()
+{
+    cout<<"**Initial Menu Here**"<<endl;
+	char menu = 'y';
+	do
+	{
+		int selection;
+		cin>>selection;
+		move(selection);
+		cout<<"Return to Menu?  y/n"<<endl;
+	}while(menu == 'y');
+}
+help::move(int navigate)
+{
+	if(navigate == 1)
+		cout<<"**Selection One**"<<endl;
+	if(navigate == 2)
+		cout<<"**Selection Two**"<<endl;
+	if(navigate == 3)
+		cout<<"**Selection Three**"<<endl;
+	/*
+	 * Add more if needed
+	 */
+}
