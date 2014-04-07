3a8afe9fb91a9dfecbf39bdffd4ef763a6fac284
dezayasa <dezayasa@ufl.edu>
2014-04-07 14:05:24 -0400

    Create Fraction.cpp

---
+++ Fraction.cpp
@@ -0,0 +1,26 @@
+#include "Fraction.h"
+Fraction::Fraction()
+{
+
+}
+Fraction::Fraction(int numer, int denom)
+{
+    numerator = numer;
+    denominator = denom;
+}
+
+/*
+ 
+ double fraction = numerator/denominator;
+ for(int d = 1; denominator > d; d++)
+ {
+ 	 for(int n=0; numerator > n; n++)
+ 	 {
+ 	 	 double temp = n/d;
+ 	 	 if(temp == fraction);
+ 	 	 denominator = d;
+ 	 	 numerator = n; 
+ 	 }
+ )
+ 
+ */
