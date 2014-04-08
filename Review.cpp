d8cbb60ca6c82d02715568387b2bb85c1edfe11a
tsaavedra23 <tsaavedra0492@gmail.com>
2014-04-08 15:31:13 -0400

    this added return values to Review.cpp

--- Review.cpp
+++ Review.cpp
@@ -1,15 +1,17 @@
 #include "Review.h"
+
+
 string Review::getExpression() {
-    
+	return expression.front()
 }
 string Review::getExpression(int index) {
-    
+	return answers.At(index)
 }
 string Review::getAnswer() {
-    
+	return answers.front()
 }
 string Review::getAnswer(int index) {
-    
+	return answers.At(index)
 }
 string Review::getAll() {
     
