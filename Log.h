4a99b8436e188dc074889bba235fe89c00a371b5
Michelle <memamdie@ufl.edu>
2014-04-10 14:46:30 -0400

    its compilable now

--- Log.h
+++ Log.h
@@ -3,16 +3,16 @@
 #include <iostream>
 #include <string>
 #include <exception>
+#include <sstream>
 
 using namespace std;
-class Log
-
-{
+class Log {
 private:
 	double base;
 	double number;
 public:
 	Log(string);
+	Log(string, string);
 	//Will this generalize and be compatible with all number types? Log of a log solved?
 	//Log(Number a, Number b) : base(a), pure(b) {}
     Log(double, double);
