426e787fa55b57a192a6dcb60c48a5d021f448b6
michelle <memamdie@ufl.edu>
2014-04-10 11:19:29 -0400

    "Added helper methods to determine the type of number"

--- Number.h
+++ Number.h
@@ -1,22 +1,33 @@
-#include <string>
+#ifndef NUMBER_H
+#define NUMBER_H
 #include "Fraction.h"
+#include <iostream>
+#include <string>
 using namespace std;
-
-
 class Number {
-    Number(string);
-    virtual Number& operator+(Number&)=0;
-    virtual Number& operator-(Number&)=0;
-    virtual Number& operator*(Number&)=0;
-    virtual Number& operator/(Number&)=0;
+    private:
+        string num;
+    public:
+        Number(string);
+        bool isPolynomial(string);
+        bool isIrrational(string);
+        bool isInteger(string);
+        bool isFraction(string);
+        bool isLog(string);
+        /*virtual Number& operator+(Number&)=0;
+        virtual Number& operator-(Number&)=0;
+        virtual Number& operator*(Number&)=0;
+        virtual Number& operator/(Number&)=0;
+        virtual Number* simplify(Number*, Number*) = 0;*/
 
 };
-
+#endif
 /*class Polynomial : Number {
 
 private:
 	string expression;
-	Polynomial(string a) : expression(a) {}
+
+	Polynomial(string a) : Expression(a) {}
 
 	Polynomial operator+(Polynomial&);
 	Polynomial operator-(Polynomial&);
@@ -35,82 +46,26 @@ class Irrational : Number {
 class Integer : Number {
 
 	private:
-		double pure;
+		int pure;
 
 	Integer operator+(Integer&);
 	Integer operator-(Integer&);
 	Integer operator*(Integer&);
 	Integer operator/(Integer&);
-};
-class Log : Number {
+};*/
+
+/*class Log : Number {
 
 	private:
 	double base;
 	double pure;
 
 	Log() : base(0), pure(0) {}
-	Log(Number a, Number* b) : base(a), pure(b) {}			//Will this generalize and be compatible with all number types? Log of a log solved?
+	Log(Number a, Number b) : base(a), pure(b) {}			//Will this generalize and be compatible with all number types? Log of a log solved?
 
 	Log operator+(Log&);
 	Log operator-(Log&);
 	Log operator*(Log&);
 	Log operator/(Log&);
-};
-=======
+};*/
 
-
-class Number {
-    Number(string);
-    virtual Number& operator+(Number&)=0;
-    virtual Number& operator-(Number&)=0;
-    virtual Number& operator*(Number&)=0;
-    virtual Number& operator/(Number&)=0;
-
-};
-
-/*class Polynomial : Number {
-
-private:
-	string expression;
-	Polynomial(string a) : expression(a) {}
-
-	Polynomial operator+(Polynomial&);
-	Polynomial operator-(Polynomial&);
-	Polynomial operator*(Polynomial&);
-	Polynomial operator/(Polynomial&);
-};
-
-class Irrational : Number {
-
-	Irrational operator+(Irrational&);
-	Irrational operator-(Irrational&);
-	Irrational operator*(Irrational&);
-	Irrational operator/(Irrational&);
-};
-
-class Integer : Number {
-
-	private:
-		double pure;
-
-	Integer operator+(Integer&);
-	Integer operator-(Integer&);
-	Integer operator*(Integer&);
-	Integer operator/(Integer&);
-};
-class Log : Number {
-
-	private:
-	double base;
-	double pure;
-
-	Log() : base(0), pure(0) {}
-	Log(Number a, Number* b) : base(a), pure(b) {}			//Will this generalize and be compatible with all number types? Log of a log solved?
-
-	Log operator+(Log&);
-	Log operator-(Log&);
-	Log operator*(Log&);
-	Log operator/(Log&);
-};
->>>>>>> d491b34338c09518a205058378e6c222d1a22a53
-*/
