1df634f472d2c2ae1dc50d8f86b73ecb0f0f3e3c
tsaavedra23 <tsaavedra0492@gmail.com>
2014-04-13 18:27:58 -0400

    updated Irrational.h

--- Irrational.h
+++ Irrational.h
@@ -2,9 +2,8 @@
 #define IRRATIONAL_H
 #include "Number.h"
 
-using namespace std;
 
-class Irrational: public Number{
+class Irrational: public Number {
 private:
     string coefficient;
     string PI;
@@ -14,24 +13,13 @@ private:
 public:
     Irrational();
     Irrational(string);
-    //~Irrational();
-    //Number* Add(string);
-    /*string addSubtractPi();
-    string multiplyPi();			Sorry, But what is this?
-    string addSubtractE();
-    string multiplyE();*/
-	Number* add(Number*);
-	Number* subtract(Number*);
-	Number* multiply(Number*);
-	Number* divide(Number*);
-
-   // ~Irrational();
-   /* Number* Add(string);
-    Number* addPi(Number* num);
-    Number* subtractPi(Number* num);
-    Number* multiplyPi(Number* num);
-    Number* addE(Number* num);
-    Number* subtractE(Number* num);
-    Number* multiplyE(Number* num);*/
+    ~Irrational();
+    string getExponent();
+    string getCoefficient();
+    string toString();
+    Number* Add(string);
+    Number* add(Number* num);
+    Number* subtract(Number* num);
+    Number* multiply(Number* num);
 };
 #endif
