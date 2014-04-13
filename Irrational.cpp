716d686297e5d61169dfdccdb271e0bc6e991999
tsaavedra23 <tsaavedra0492@gmail.com>
2014-04-13 18:27:08 -0400

    updated irrational .cpp

--- Irrational.cpp
+++ Irrational.cpp
@@ -1,13 +1,14 @@
 #include "Irrational.h"
-
-using namespace std; 
-
 Irrational::Irrational() {
 
    }
 Irrational::Irrational(string str) {
     int i = 0;
+    PI = "";
+    e = "";
     coefficient = "";
+    exponent= "";
+    typeofIrrational = "";
     int temp;
     while(i < str.length()) {
          if(istringstream(str.substr(i, 1)) >> temp) {
@@ -16,11 +17,29 @@ Irrational::Irrational(string str) {
         }
          else if(!(istringstream(str.substr(i, 1)) >> temp)) {
             if((str[i] == 'p' && str[i+1] == 'i') || (str[i] == 'P' && str[i+1] == 'i') || (str[i] == 'p' && str[i+1] == 'I') || (str[i] == 'P' && str[i+1] == 'I')) {
-                PI = coefficient + "pi";
+                if(str.substr(i+2,1) == "^") {
+                    for(int j = i; j < str.length(); j++) {
+                        exponent += str[j+2];
+                    }
+                    typeofIrrational = coefficient + "pi" + "^"+ exponent;
+                    PI = "pi";
+                    break;
                 }
+                typeofIrrational = coefficient + "pi";
+                PI = "pi";
+            }
             else if((str[i] == 'e' || str[i] == 'E')) {
-                e = coefficient + "e";
+                if(str.substr(i+1,1) == "^") {
+                    for(int j = i; j < str.length(); j++) {
+                        exponent += str[j+2];
+                    }
+                    typeofIrrational = coefficient + "e" + "^"+ exponent;
+                    e = "e";
+                    break;
                 }
+                typeofIrrational = coefficient + "e";
+                e = "e";
+            }
             i = str.length();
          }
          
@@ -29,50 +48,31 @@ Irrational::Irrational(string str) {
          }
     }
     
+    
+}
+string Irrational::getExponent() {
+    return exponent;
+}
+string Irrational::getCoefficient() {
+    return coefficient;
+}
+string Irrational::toString() {
+    return typeofIrrational;
 }
 
-/*Irrational::~Irrational() {
-	delete this;
-	//delete e;
-	//delete typeofIrrational;
-}*/
 
-Number* Irrational::add(Number* a) {
-	Number * b = new Irrational("3pi");
-	return b;
-}
-
-Number* Irrational::subtract(Number* a) {
-	Number * b = new Irrational("3pi");
-	return b;
-}
-
-Number* Irrational::multiply(Number* a) {
-	Number * b = new Irrational("3pi");
-	return b;
-}
-
-Number* Irrational::divide(Number* a) {
-	Number * b = new Irrational("3pi");
-	return b;
-}
-
-/*
-string Irrational::addSubtractPi() {
-
-
-Number* Irrational::addPi(Number* num) {
-    Number* res = new Number();
+Number* Irrational::add(Number* num) {
     int i, temp, tempcoeff;
     string Num = num->getNumber();
     string tempString = "";
     string result = "";
-    while(i < Num.length()){
-        if(istringstream(Num.substr(i, 1)) >> temp) {
-            tempString += Num[i];
-            i++;
-        }
-        else if(!(istringstream(Num.substr(i, 1)) >> temp)){
+    if(PI != "") {
+        while(i < Num.length()){
+            if(istringstream(Num.substr(i, 1)) >> temp) {
+                tempString += Num[i];
+                i++;
+            }
+            else if(!(istringstream(Num.substr(i, 1)) >> temp)){
                 istringstream(tempString) >> temp;
                 if((Num[i] == 'p' && Num[i+1] == 'i') || (Num[i] == 'P' && Num[i+1] == 'i') || (Num[i] == 'p' && Num[i+1] == 'I') || (Num[i] == 'P' && Num[i+1] == 'I')){
                     istringstream(coefficient) >> tempcoeff;
@@ -88,88 +88,16 @@ Number* Irrational::addPi(Number* num) {
                     break;
                 }   
         }
-        result = Num + "+pi";
+            result = Num + "+pi";
+        }
     }
-    res->setNumber(result);
-    return res;
-
-}
-Number* Irrational::subtractPi(Number* num) {
-    Number* res = new Number();
-    int i, temp, tempcoeff;
-    string Num = num->getNumber();
-    string tempString;
-    string result = "";
-    while(i < Num.length()){
-        if(istringstream(Num.substr(i, 1)) >> temp) {
-            tempString += Num[i];
-            i++;
-        }
-        else if(!(istringstream(Num.substr(i, 1)) >> temp)){
-                istringstream(tempString) >> temp;
-                if((Num[i] == 'p' && Num[i+1] == 'i') || (Num[i] == 'P' && Num[i+1] == 'i') || (Num[i] == 'p' && Num[i+1] == 'I') || (Num[i] == 'P' && Num[i+1] == 'I')){
-                    istringstream(coefficient) >> tempcoeff;
-                    temp -= tempcoeff;
-                    ostringstream(tempString) << temp;
-                    result = tempString + "pi";
-                    i = Num.length();
-                    break;
-                }
-                else{
-                    result = Num + "-pi";
-                    i = Num.length();
-                    break;
-                }   
-        }
-        result = Num + "-pi";
-    }
-    res->setNumber(result);
-    return res;
-}
-Number* Irrational::multiplyPi(Number* num) {
-    Number* res = new Number();
-    int i, temp, tempcoeff;
-    string Num = num->getNumber();
-    string tempString = "";
-    string result = "";
-    while(i < Num.length()){
-        if(istringstream(Num.substr(i, 1)) >> temp) {
-            tempString += Num[i];
-            i++;
-        }
-        else if(!(istringstream(Num.substr(i, 1)) >> temp)){
-                istringstream(tempString) >> temp;
-                if((Num[i] == 'p' && Num[i+1] == 'i') || (Num[i] == 'P' && Num[i+1] == 'i') || (Num[i] == 'p' && Num[i+1] == 'I') || (Num[i] == 'P' && Num[i+1] == 'I')){
-                    istringstream(coefficient) >> tempcoeff;
-                    temp *= tempcoeff;
-                    ostringstream(tempString) << temp;
-                    result = tempString + "pi^2";
-                    i = Num.length();
-                    break;
-                }
-                else{
-                    result = Num + "pi";
-                    i = Num.length();
-                    break;
-                }   
-        }
-        result = Num + "pi";
-    }
-    res->setNumber(result);
-    return res;
-}
-Number* Irrational::addE(Number* num) {
-    Number* res = new Number();
-    int i, temp, tempcoeff;
-    string Num = num->getNumber();
-    string tempString = "";
-    string result = "";
-    while(i < Num.length()){
-        if(istringstream(Num.substr(i, 1)) >> temp) {
-            tempString += Num[i];
-            i++;
-        }
-        else if(!(istringstream(Num.substr(i, 1)) >> temp)){
+    else if(e != "") {
+        while(i < Num.length()){
+            if(istringstream(Num.substr(i, 1)) >> temp) {
+                tempString += Num[i];
+                i++;
+            }
+            else if(!(istringstream(Num.substr(i, 1)) >> temp)){
                 istringstream(tempString) >> temp;
                 if((Num[i] == 'e' || Num[i] == 'E')){
                     istringstream(coefficient) >> tempcoeff;
@@ -184,24 +112,50 @@ Number* Irrational::addE(Number* num) {
                     i = Num.length();
                     break;
                 }   
+            }
+            result = Num + "+e";
         }
-        result = Num + "+e";
     }
-    res->setNumber(result);
-    return res;
+    num->setNumber(result);
+    return num;  
 }
-Number* Irrational::subtractE(Number* num) {
-    Number* res = new Number();
+Number* Irrational::subtract(Number* num) {
     int i, temp, tempcoeff;
     string Num = num->getNumber();
-    string tempString;
+    string tempString = "";
     string result = "";
-    while(i < Num.length()){
-        if(istringstream(Num.substr(i, 1)) >> temp) {
-            tempString += Num[i];
-            i++;
+    if(PI != "") {
+        while(i < Num.length()){
+            if(istringstream(Num.substr(i, 1)) >> temp) {
+                tempString += Num[i];
+                i++;
+            }
+            else if(!(istringstream(Num.substr(i, 1)) >> temp)){
+                istringstream(tempString) >> temp;
+                if((Num[i] == 'p' && Num[i+1] == 'i') || (Num[i] == 'P' && Num[i+1] == 'i') || (Num[i] == 'p' && Num[i+1] == 'I') || (Num[i] == 'P' && Num[i+1] == 'I')){
+                    istringstream(coefficient) >> tempcoeff;
+                    temp -= tempcoeff;
+                    ostringstream(tempString) << temp;
+                    result = tempString + "pi";
+                    i = Num.length();
+                    break;
+                }
+                else{
+                    result = Num + "-pi";
+                    i = Num.length();
+                    break;
+                }   
+            }
+            result = Num + "-pi";
         }
-        else if(!(istringstream(Num.substr(i, 1)) >> temp)){
+    }
+    else if(e != "") {
+        while(i < Num.length()){
+            if(istringstream(Num.substr(i, 1)) >> temp) {
+                tempString += Num[i];
+                i++;
+            }
+            else if(!(istringstream(Num.substr(i, 1)) >> temp)){
                 istringstream(tempString) >> temp;
                 if((Num[i] == 'e' || Num[i] == 'E')){
                     istringstream(coefficient) >> tempcoeff;
@@ -216,15 +170,44 @@ Number* Irrational::subtractE(Number* num) {
                     i = Num.length();
                     break;
                 }   
+            }
+            result = Num + "-e";
         }
-        result = Num + "+e";
     }
-    res->setNumber(result);
-    return res;
+    num->setNumber(result);
+    return num;
 }
-Number* Irrational::multiplyE(Number* num) {
-
+Number* Irrational::multiply(Number* num) {
+    int i, temp, tempcoeff,tempExp;
+    string Num = num->getNumber();
+    string tempString = "";
+    string result = "";
+    if(PI != "") {
+        while(i < Num.length()){
+            if(istringstream(Num.substr(i, 1)) >> temp) {
+                tempString += Num[i];
+                i++;
+            }
+            else if(!(istringstream(Num.substr(i, 1)) >> temp)){
+                istringstream(tempString) >> temp;
+                if((Num[i] == 'p' && Num[i+1] == 'i') || (Num[i] == 'P' && Num[i+1] == 'i') || (Num[i] == 'p' && Num[i+1] == 'I') || (Num[i] == 'P' && Num[i+1] == 'I')){
+                    istringstream(coefficient) >> tempcoeff;
+                    
+                    temp *= tempcoeff;
+                    ostringstream(tempString) << temp;
+                    result = tempString + "pi^2";
+                    i = Num.length();
+                    break;
+                }
+                else{
+                    result = Num + "pi";
+                    i = Num.length();
+                    break;
+                }
+            }
+            result = Num + "pi";
+        }
+    }
+    num->setNumber(result);
+    return num;
 }
-*/
-
-
