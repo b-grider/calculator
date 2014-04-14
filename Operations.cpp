0a8c2acbdedafd268754c7ab1875304d1b1f6092
tsaavedra23 <tsaavedra0492@gmail.com>
2014-04-14 01:45:48 -0400

    added the divide function
    it is not fully functional but the irrational division is taking shape

--- Operations.cpp
+++ Operations.cpp
@@ -592,3 +592,271 @@ string subtract(string left, string right) {
     }
     return answer;
 }
+
+string divide(string left, string right) {
+    string leftCoefficient, rightCoefficient, leftExponent, rightExponent, finalC,finalE, answer, simplifiedLeft, simplifiedRight;
+    int leftC, rightC,leftE, rightE, final, leftO, rightO;
+    //checks if the left and right operands are of the same type
+    if ((isIrrational(left) && isIrrational(right)) || (isFraction(left) && isFraction(right)) || (isInteger(left) && isInteger(right)) || (isLog(left) && isLog(right)) || (isPolynomial(left) && isPolynomial(right))) {
+        //Dealing with Irrational Numbers
+        if (isIrrational(left)) {
+            int i = 0;
+            leftCoefficient = "";
+            leftExponent = "";
+            int temp;
+
+            while (i < left.length()) {
+                if (istringstream(left.substr(i, 1)) >> temp) {
+                    leftCoefficient += left[i];
+
+                } else
+                    break;
+                i++;
+            }
+            if (leftCoefficient == "") {
+                leftCoefficient = "1";
+            }
+            i = 0;
+            rightCoefficient = "";
+            rightExponent = "";
+            while (i < right.length()) {
+                if (istringstream(right.substr(i, 1)) >> temp) {
+                    rightCoefficient += right[i];
+
+                } else
+                    break;
+                i++;
+            }
+            if (rightCoefficient == "") {
+                rightCoefficient = "1";
+            }
+
+
+            istringstream(leftCoefficient) >> leftC;
+            istringstream(rightCoefficient) >> rightC;
+            if ((leftC % rightC) == 0) {
+                final = leftC / rightC;
+                ostringstream temporary;
+                temporary << final;
+                finalC = temporary.str();
+                if (isPi(left) && isPi(right)) {
+                    while (i < left.length()) {
+                        if (left.substr(i, 1) == "^") {
+                            leftExponent += left[i + 1];
+                        }
+                        i++;
+                    }
+                    if(leftExponent == "") {
+                        leftExponent == "0";
+                    }
+                    i = 0;
+                    while (i < right.length()) {
+                        if (right.substr(i, 1) == "^") {
+                            rightExponent += right[i + 1];
+                        }
+                        i++;
+                    }
+                    if(rightExponent == "") {
+                        rightExponent == "0";
+                    }
+                    istringstream(leftExponent) >> leftE;
+                    istringstream(rightExponent) >> rightE;
+                    final = leftE - rightE;
+                    ostringstream temporary;
+                    temporary << final;
+                    finalE = temporary.str();
+                    if (final == 0) {
+                        answer = finalC;
+                    } else
+                        answer = finalC + "pi" + "^" + finalE;
+                } else if (isE(left) && isE(right)) {
+                    while (i < left.length()) {
+                        if (left.substr(i, 1) == "^") {
+                            leftExponent += left[i + 1];
+                        }
+                        i++;
+                    }
+                    if(leftExponent == "") {
+                        leftExponent == "0";
+                    }
+                    i = 0;
+                    while (i < right.length()) {
+                        if (right.substr(i, 1) == "^") {
+                            rightExponent += right[i + 1];
+                        }
+                        i++;
+                    }
+                    if(rightExponent == "") {
+                        rightExponent == "0";
+                    }
+                    istringstream(leftExponent) >> leftE;
+                    istringstream(rightExponent) >> rightE;
+                    final = leftE - rightE;
+                    ostringstream temporary;
+                    temporary << final;
+                    finalE = temporary.str();
+                    if (final == 0) {
+                        answer = finalC;
+                    } else
+                        answer = finalC + "e" + "^" + finalE;
+                } else if (isPi(left) && isE(right)) {
+                    if(leftE == 0 && rightE == 0){
+                        answer = finalC + "pi" + "/" + "e";
+                    }
+                    else
+                        answer = finalC + "pi" + "^" + leftExponent + "/" + "e" + "^" + rightExponent;
+                }
+                else if(isE(left) && isPi(right)) {
+                    if(leftE == 0 && rightE == 0)
+                        answer = finalC + "e" + "/" + "i";
+                    else
+                        answer = finalC + "e" + "^" + leftExponent + "/" + "pi" + "^" + rightExponent;
+                }
+            }
+
+
+
+        } else if (isLog(left)) {
+            string leftBase, rightBase, leftNum, rightNum, final;
+            int i = 4;
+            //Specified base for the log
+            if (left[3] == '_') {
+                while (left[i] != ':') {
+                    leftBase += left[i];
+                    i++;
+                }
+                i++;
+                while (i <= left.length() - 1) {
+                    leftNum += left[i];
+                    i++;
+                }
+            }//This is log base 10
+            else if (left[3] == ':') {
+                leftBase = "10";
+                while (i <= left.length() - 1) {
+                    leftNum += left[i];
+                    i++;
+                }
+            }
+            i = 4;
+            if (right[3] == '_') {
+                while (right[i] != ':') {
+                    rightBase += right[i];
+                    i++;
+                }
+                i++;
+                while (i <= right.length() - 1) {
+                    rightNum += right[i];
+                    i++;
+                }
+            }//This is log base 10
+            else if (right[3] == ':') {
+                rightBase = "10";
+                while (i <= right.length() - 1) {
+                    rightNum += right[i];
+                    i++;
+                }
+            }
+            int num, tempLeft, tempRight, tempLNum, tempRNum;
+            istringstream(leftBase) >> tempLeft;
+            istringstream(rightBase) >> tempRight;
+            istringstream(leftNum) >> tempLNum;
+            istringstream(rightNum) >> tempRNum;
+            simplifiedLeft = simplifyLog(tempLeft, tempLNum);
+            simplifiedRight = simplifyLog(tempRight, tempRNum);
+            if (isInteger(simplifiedLeft)) {
+                left = simplifiedLeft;
+                istringstream(left) >> leftO;
+            }
+            if (isInteger(simplifiedRight)) {
+                right = simplifiedRight;
+                istringstream(left) >> rightO;
+            }
+            num = tempLNum / tempRNum;
+            if (isInteger(simplifiedLeft) && isInteger(simplifiedRight)) {
+                answer = leftO - rightO;
+            } else if (!isInteger(simplifiedLeft) && !isInteger(simplifiedRight)) {
+                if (tempLeft == tempRight) {
+                    string a = simplifyLog(tempLeft, num);
+                    answer = "log_" + leftBase + ":" + a;
+                }
+            } else {
+                answer = left + " - " + right;
+            }
+
+
+        } else if (isFraction(left)) {
+            string numerator, denominator;
+            int i = 0;
+            string topLeft, topRight, bottomLeft, bottomRight;
+            int leftNum, rightNum, leftDenom, rightDenom, finalNum;
+            while (left[i] != '/') {
+                topLeft += left[i];
+                i++;
+            }
+            i++;
+            while (i < left.length()) {
+                bottomLeft += left[i];
+                i++;
+            }
+            i = 0;
+            while (right[i] != '/') {
+                topRight += right[i];
+                i++;
+            }
+            i++;
+            while (i < right.length()) {
+                bottomRight += right[i];
+                i++;
+            }
+            istringstream(topLeft) >> leftNum;
+            istringstream(topRight) >> rightNum;
+            istringstream(bottomLeft) >> leftDenom;
+            istringstream(bottomRight) >> rightDenom;
+            //if the fractions can be simplified
+            int d, e;
+            d = gcd(leftNum, leftDenom);
+            leftNum /= d;
+            leftDenom /= d;
+            e = gcd(rightNum, rightDenom);
+            rightNum /= e;
+            rightDenom /= e;
+            if (leftDenom == rightDenom) {
+                finalNum = leftNum - rightNum;
+                ostringstream temp;
+                temp << finalNum;
+                numerator = temp.str();
+                denominator = bottomLeft;
+            } else {
+                leftNum *= rightDenom;
+                rightNum *= leftDenom;
+                leftDenom *= rightDenom;
+                finalNum = leftNum - rightNum;
+                ostringstream numer;
+                numer << finalNum;
+                numerator = numer.str();
+                ostringstream denom;
+                denom << leftDenom;
+                denominator = denom.str();
+
+            }
+            answer = numerator + "/" + denominator;
+        }
+        if (isPolynomial(left)) {
+
+        }
+        if (isInteger(left)) {
+            int leftO, rightO, ans;
+            istringstream(left) >> leftO;
+            istringstream(right) >> rightO;
+            ans = leftO - rightO;
+            ostringstream t;
+            t << ans;
+            answer = t.str();
+        }
+
+    } else {
+        answer = left + " - " + right;
+    }
+    return answer;
+}
